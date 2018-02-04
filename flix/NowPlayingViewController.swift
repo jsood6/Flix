6104db12cfc6d4f9789077a837de0a3e3f5aee66
Jigyasaa Sood <jsood@ucsd.edu>
2018-02-04 00:52:51 -0800

    getting a bug

---
+++ flix/NowPlayingViewController.swift
@@ -0,0 +1,45 @@
+//
+//  NowPlayingViewController.swift
+//  flix
+//
+//  Created by jsood on 2/4/18.
+//  Copyright © 2018 Jigyasaa Sood. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=<<api_key>>")!
+        
+        //getting data
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        //task to get the data
+        let task = session.dataTask(with: request) { (data, response, error) in
+            //This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+                print(dataDictionary)
+            }
+        }
+        //ALWAYS NEED TO CALL THIS FUNCTION! this will actually start the task
+        task.resume()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
