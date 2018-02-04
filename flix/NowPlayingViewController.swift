146ed26d35944107953f6f8cebfdd94ac4bae2d1
Jigyasaa Sood <jsood@ucsd.edu>
2018-02-04 02:59:23 -0800

    fixed bug, required assignment done

--- flix/NowPlayingViewController.swift
+++ flix/NowPlayingViewController.swift
@@ -7,13 +7,37 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class NowPlayingViewController: UIViewController {
-
+class NowPlayingViewController: UIViewController, UITableViewDataSource{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies: [[String:Any]] = []
+    var refreshControl: UIRefreshControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.rowHeight = 150
         
-        let url = URL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=<<api_key>>")!
+        refreshControl = UIRefreshControl()
+        //action is what method is called and start enums by .
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        
+        tableView.insertSubview(refreshControl, at: 0)
+        tableView.dataSource = self
+        fetchMovies()
+    }
+    
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl)
+    {
+        fetchMovies()
+        
+    }
+    
+    func fetchMovies()
+    {
+        let url = URL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         
         //getting data
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -29,11 +53,41 @@ class NowPlayingViewController: UIViewController {
             else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
                 print(dataDictionary)
+                let movies = dataDictionary["results"] as! [[String:Any]]
+                self.movies = movies
+                self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
+                
             }
         }
         //ALWAYS NEED TO CALL THIS FUNCTION! this will actually start the task
         task.resume()
+    }
+    
+    //how many cells
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //count is the number of movies in our dictionary
+        return movies.count
+    }
+    
+    //what the cell is going to be
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
         
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
+        let posterPathString = movie["poster_path"] as! String
+        let baseURLString = "https://image.tmdb.org/t/p/w500"
+        
+        let posterURL = URL(string: baseURLString + posterPathString)!
+        cell.posterImage.af_setImage(withURL: posterURL)
+        
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
